4e8bcf2c10ad4daf6ec29749092a1804b1d5b3e9
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-11-09 10:37:55 -0800

    finish hooks

--- HOOKS/src/context/index.jsx
+++ HOOKS/src/context/index.jsx
@@ -1,20 +1,38 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
+
 
 const MyContext = createContext();
-
 const MyProvider = (props) => {
-
-    const [users, setUsers] = useState([
+    const [active,setActive] = useState(true);
+    const [users,setUsers] = useState([
         {id:1,name:"Francis"},
         {id:2,name:"Steve"},
         {id:3,name:"Miles"}
     ])
 
-    return (
-        <MyContext.Provider value={users}>
-            {props.children}
+
+    const addUser = (name) => {
+        setUsers(prevState => (
+            [
+                ...prevState,
+                {id:4,name:name}
+            ]
+        ))
+    }
+
+
+    return(
+        <MyContext.Provider value={{
+            users:users,
+            addUser:addUser,
+            setUsers:setUsers,
+            activeState:active,
+            setActive: () => setActive(!active)
+        }}>
+            { props.children }
         </MyContext.Provider>
     )
 }
 
-export {MyContext, MyProvider}
+
+export {MyContext,MyProvider}
