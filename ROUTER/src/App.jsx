d4ce04585f991094787b311b4176fe3ffe211593
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-11-13 15:02:19 -0800

    linking

--- ROUTER/src/App.jsx
+++ ROUTER/src/App.jsx
@@ -1,21 +1,27 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link  } from 'react-router-dom';
 
-import Home from "./components/home";
-import Posts from "./components/posts";
-import Profile from "./components/profile";
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profile';
+
 
 const App = () => {
-
   return(
     <BrowserRouter>
-    <header>
-      Header
-    </header>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='posts' element={<Posts/>}/>
-      <Route path='profile' element={<Profile/>}/>
-    </Routes>
+      <header>
+        <Link to="/">Home</Link><br/>
+        <Link to="posts">Posts</Link><br/>
+        <Link to={{
+          pathname:'profile',
+          hash:'#francis',
+          search:'?profile=true'
+        }}>Profile</Link>
+      </header>
+      <Routes>
+        <Route path='posts' element={<Posts/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </BrowserRouter>
   )
 }
--- ROUTER/src/components/profile.jsx
+++ ROUTER/src/components/profile.jsx
@@ -1,10 +1,18 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
+
 const Profile = () => {
+  let navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  //http://localhost:5173/profile?profile=true#francis
+  console.log(searchParams)
 
     return(
       <>
-      <h1>Profile</h1>
+        <div>Profile</div>
+        <button onClick={()=> navigate('/')}>Back home</button>
       </>
     )
   }
   
-  export default Profile;
+export default Profile;
