fea56c7b295111cdc940d8b3436a94a020714734
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-11-13 11:40:00 -0800

    router

---
+++ ROUTER/src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Home from "./components/home";
+import Posts from "./components/posts";
+import Profile from "./components/profile";
+
+const App = () => {
+
+  return(
+    <>
+    <h1>App</h1>
+    </>
+  )
+}
+
+export default App;
