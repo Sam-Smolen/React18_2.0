7e4e4454a564e7f69a7427eb90fa75d236932dc8
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-11-14 11:35:25 -0800

    more with router

--- ROUTER/src/App.jsx
+++ ROUTER/src/App.jsx
@@ -1,27 +1,63 @@
-import { BrowserRouter, Routes, Route, Link  } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, HashRouter, MemoryRouter, NavLink, Navigate } from 'react-router-dom';
 
 import Home from './components/home';
 import Posts from './components/posts';
 import Profile from './components/profile';
+import PostItem from './components/postitem';
+
+import Users from './components/users';
+import Guests from './components/guests';
+import Admins from './components/admins';
 
 
 const App = () => {
   return(
     <BrowserRouter>
-      <header>
-        <Link to="/">Home</Link><br/>
-        <Link to="posts">Posts</Link><br/>
-        <Link to={{
-          pathname:'profile',
-          hash:'#francis',
-          search:'?profile=true'
-        }}>Profile</Link>
-      </header>
-      <Routes>
-        <Route path='posts' element={<Posts/>}/>
-        <Route path='profile' element={<Profile/>}/>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
+      <div className='container'>
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <Link to="/" className='d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none'>
+            <span className='fs-4'>My app</span>
+          </Link>
+
+          <ul className='nav nav-pills'>
+            <li className='nav-item'>
+              <NavLink to="/" className='nav-link'>
+                Home
+              </NavLink>
+              {/* <Link to="/" className='nav-link'>Home</Link> */}
+            </li>
+            <li className='nav-item'>
+              <NavLink to="posts" className='nav-link'>Posts</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink to="profile" className='nav-link'>Profile</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink to="users" className='nav-link'>Users</NavLink>
+            </li>
+          </ul>
+        </header>
+        <Routes>
+          <Route path='posts' element={<Posts/>}/>
+          <Route path='posts/:id' element={<PostItem/>}/>
+          <Route path='profile' element={<Profile/>}/>
+          {/* <Route path='profile' element={
+            <>
+               <Navigate replace to="/"/>
+            </>
+          }/> */}
+          <Route path="users" element={<Users/>}>
+            <Route path='guests' element={<Guests/>}/>
+            <Route path='admins' element={<Admins/>}/>
+          </Route>
+          <Route path='/' element={<Home/>}/>
+          <Route path="*" element={
+            <>
+                <h1>Sorry nothing found</h1>
+            </>
+          }/>
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
--- ROUTER/src/components/profile.jsx
+++ ROUTER/src/components/profile.jsx
@@ -1,16 +1,17 @@
-import { useNavigate, useSearchParams } from 'react-router-dom'
+import { useNavigate, Navigate, useSearchParams } from 'react-router-dom'
 
 const Profile = () => {
   let navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
   //http://localhost:5173/profile?profile=true#francis
-  console.log(searchParams)
+  console.log(searchParams.get('profile'))
 
     return(
       <>
         <div>Profile</div>
         <button onClick={()=> navigate('/')}>Back home</button>
+        <Navigate replace to="/"/>
       </>
     )
   }
