387c916cd6c644c122078b795c8d37860cf9774a
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-11-17 11:24:53 -0800

    async requests

---
+++ REDUX/src/store/users.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+
+export const fetchUser = createAsyncThunk(
+    'users/fetchUser',
+    async(thunkAPI)=>{
+        const res = await axios.get('https://jsonplaceholder.typicode.com/users').then(
+            (response) => response.data
+        );
+        return res;
+    }
+)
+
+
+export const usersSlice = createSlice({
+    name:'users',
+    initialState:{
+        type:'Guest',
+        users:[]
+    },
+    reducers:{
+        setType:(state,action)=>{
+            state.type = action.payload || 'Guest'
+        }
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchUser.pending,(state)=>{
+            console.log('pending')
+        })
+        .addCase(fetchUser.fulfilled,(state,action)=>{
+            console.log('fulfilled');
+            // console.log(action.payload)
+            state.users = action.payload;
+        })
+        .addCase(fetchUser.rejected,(state)=>{
+            console.log('rejected');
+        })
+    }
+});
+
+export const { setType } = usersSlice.actions
+export default usersSlice.reducer;
