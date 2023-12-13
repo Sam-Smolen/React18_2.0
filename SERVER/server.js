871fb8cbe8d2ed4d9a6467c9e064b541ced74020
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-13 10:49:50 -0800

    express server

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,36 +1,12 @@
-// Building a server with node js
+// express server 
+const express = require('express');
+const app = express();
 
-const http = require('http');
-const fs = require('fs');
+const PORT = 3333;
 
-const names = ['francis','james','rob'];
-const cars = {
-    name:"Ford",
-    model:"Fiesta"
-}
+app.get('/',(req, res)=> {
+    res.send('This is the homepage');
+})
 
-const server = http.createServer(function(req,res){
-
-    if(req.url === '/'){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        const HTML = fs.readFileSync(`${__dirname}/index.html`);
-        res.end(HTML)
-    } else if(req.url === '/api/users') {
-        res.writeHead(200,{'Content-Type':'application/json'});
-        const json = JSON.stringify({
-                names,
-                cars
-            });
-            res.end(json)
-    } else if(req.url === '/api/text') {
-        res.writeHead(200,{'Content-Type':'text/plain'});
-        res.end('Welcome to my Node js server');
-    } else {
-        res.writeHead(404);
-        res.end();
-    }
-    
-});
-
-server.listen(8181,'127.0.0.1');
-console.log('Server is running on port 8181');
+app.listen(PORT);
+console.log(`Server listening on localhost:${PORT}!`);
