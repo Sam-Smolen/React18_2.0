4e83c9f3c0785f0ab7df3cf4c8fcc801328bab52
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-15 12:01:17 -0800

    more w middlewares

--- SERVER/server.js
+++ SERVER/server.js
@@ -4,12 +4,18 @@ const app = express();
 
 
 app.use('/css',express.static( __dirname + '/public/css'));
+app.use('/',(req,res,next)=>{
+    console.log('Somebody made a request for:' + req.url);
+    res.cookie('cookiename','cookievalue');
+    next();
+});
 
 
 app.get('/',(req,res)=>{
     res.send(`
         <html>
             <head>
+            <link rel="shortcut icon" href="#" />
                 <link type="text/css" rel="stylesheet" href="/css/styles.css">
             </head>
             <body>
