315ae312dcc5b9a24f88c233aea81f0123a450b3
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-16 11:24:22 -0800

    Posting Data

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,9 +1,12 @@
-// express server
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs')
 
-
-app.use('/css',express.static( __dirname + '/public/css'));
+app.use('/css',express.static( __dirname+ '/public/css'));
+// app.use(bodyParser.json());
+const jsonParser = bodyParser.json();
+const urlencodeParser = bodyParser.urlencoded({extended:false});
 app.use('/',(req,res,next)=>{
     console.log('Somebody made a request for:' + req.url);
     res.cookie('cookiename','cookievalue');
@@ -15,7 +18,6 @@ app.get('/',(req,res)=>{
     res.send(`
         <html>
             <head>
-            <link rel="shortcut icon" href="#" />
                 <link type="text/css" rel="stylesheet" href="/css/styles.css">
             </head>
             <body>
@@ -24,23 +26,54 @@ app.get('/',(req,res)=>{
         </html>
     `)
 });
+
+
+app.get('/user',(req,res)=>{
+    let HTML = fs.readFileSync(`${__dirname}/views/user.html`);
+    res.send(`${HTML}`)
+});
+
+app.get('/querystring',(req,res)=>{
+    let HTML = fs.readFileSync(`${__dirname}/views/querystring.html`);
+    res.send(`${HTML}`)
+});
+
+
+
+app.post('/api/adduser',jsonParser,(req,res)=>{
+    console.log(req.body);
+    res.sendStatus(200)
+});
+
+app.post('/api/queryadd',urlencodeParser,(req,res)=>{
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    console.log(firstname + ' ' + lastname )
+    res.sendStatus(200)
+});
+
+
+
+
+
 app.get('/api/:user/:id',(req,res)=>{
     let id = req.params.id;
     let username = req.params.user;
-    res.send(`
+     res.send(`
         <html>
             <body>
-                <h1>The username is ${username} and the ID is ${id}</h1>
+                <h1>THE USER ID IS ${id} AND THE USERNAME ${username}</h1>
             </body>
         </html>
     `)
 });
 
-// /api/car?brand=lexus&year=2001
 
-app.get('/api/car',(req,res)=> {
-    let brand = req.query.brand
-    let year = req.query.year
+///  /car?brand=ford&year=2023
+app.get('/api/car',(req,res)=>{
+    let brand =  req.query.brand;
+    let year = req.query.year;
+
     res.send({
         brand,
         year
