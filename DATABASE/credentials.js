efb81f91454841f0a574b5876a21735a8312734c
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-17 12:19:48 -0800

    connect to mongo

---
+++ DATABASE/credentials.js
@@ -0,0 +1,4 @@
+//mongo credentials 
+
+export const username = 'samsmolen'
+export const password= 'EBZHXV2CYV9sPRLz'
--- DATABASE/server.js
+++ DATABASE/server.js
@@ -1,5 +1,30 @@
 const express = require('express');
 const app = express();
+// import { username, password } from './credentials';
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//mongo credentials
+
+
+const mongoURI = `mongodb+srv://samsmolen:EBZHXV2CYV9sPRLz@cluster0.i3hzkyb.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(mongoURI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+async function run () {
+    try {
+        await client.connect()
+        console.log('connected to Mongo')
+    }finally {
+        await client.close()
+    }
+}
+run().catch(console.dir);
 
 const port = process.env.PORT || 3001;
 app.listen(port);
