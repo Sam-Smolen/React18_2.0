a660dbd4ecd1f14c184faa3945263cc97a2f15fc
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-18 11:25:05 -0800

    mongoose connection

--- DATABASE/server.js
+++ DATABASE/server.js
@@ -1,54 +1,25 @@
 const express = require('express');
 const app = express();
-// import { username, password } from './credentials';
-const { MongoClient, ServerApiVersion } = require('mongodb');
+//connect to db with mongoose
+const mongoose = require('mongoose');
+const mongoURI = `mongodb+srv://samsmolen:EBZHXV2CYV9sPRLz@cluster0.i3hzkyb.mongodb.net/?retryWrites=true&w=majority`;
 
-//mongo credentials
+mongoose.connect(mongoURI)
+console.log('mongoose connected!');
 
-
-const mongoURI = `mongodb+srv://samsmolen:EBZHXV2CYV9sPRLz@cluster0.i3hzkyb.mongodb.net/?retryWrites=true&w=majority`
-
-const client = new MongoClient(mongoURI, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
-// async function run(){
-//     try{
-//         await client.connect();
-//         console.log('connected')
-//     } finally {
-//         await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-
-
-app.get('/api/users',async(req,res)=>{
-  try{
-      await client.connect();
-      const database = client.db('myApp');
-      const collection = database.collection('users');
-      const query = await collection.insertOne({
-          name:'Francis',
-          lastname:'Jones'
-      });
-      console.log(query);
-
-      res.status(200).json({awesome:'Yes'});
-  } catch(error){
-      throw error;
-  } finally{
-      await client.close();
-      console.log('all is done')
-  }
+// SCHEMA //
+const carSchema = mongoose.Schema({
+  brand:String,
+  model:String,
+  year:Number,
+  avail:Boolean
 });
+
+const Car = mongoose.model('Car',carSchema);
+////////////
+
 
 
 const port = process.env.PORT || 3001;
 app.listen(port);
-console.log(`App running on ${port}`);
+console.log(`Server listening on port ${port}`);
