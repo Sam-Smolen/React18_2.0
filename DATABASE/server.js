324ab462932fbb28a5daf94b65734b8592b866eb
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-12-18 12:16:05 -0800

    POST request

--- DATABASE/server.js
+++ DATABASE/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 //connect to db with mongoose
 const mongoose = require('mongoose');
-const mongoURI = `mongodb+srv://samsmolen:EBZHXV2CYV9sPRLz@cluster0.i3hzkyb.mongodb.net/?retryWrites=true&w=majority`;
+const mongoURI = `mongodb+srv://samsmolen:EBZHXV2CYV9sPRLz@cluster0.i3hzkyb.mongodb.net/myApp?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoURI)
 console.log('mongoose connected!');
+
+app.use(bodyParser.json())
 
 // SCHEMA //
 const carSchema = mongoose.Schema({
@@ -18,6 +21,20 @@ const carSchema = mongoose.Schema({
 const Car = mongoose.model('Car',carSchema);
 ////////////
 
+app.post('/api/addcar',async(req,res)=>{
+  try{
+      const addCar = new Car({
+          brand: req.body.brand,
+          model: req.body.model,
+          year: req.body.year,
+          avail: req.body.avail
+      })
+      const newCar = await addCar.save();
+      res.sendStatus(200)
+  } catch(err){
+      console.log(err)
+  }
+})
 
 
 const port = process.env.PORT || 3001;
